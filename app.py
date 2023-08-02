c93de5aab58b2e9c5334e8d8aa1cd5414be7378b
Mahdi Hasan <101652192+HsN22@users.noreply.github.com>
2023-08-02 19:38:28 +0100

    first function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from functions import apology, interpolate
 
 # Configure application
 app = Flask(__name__)
@@ -11,5 +12,16 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure CS50 Library to use SQLite database
 #db = SQL("sqlite:///databaseName.db")
+# @app.route("/login", methods=["GET", "POST"])
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+def specific():
+    if request.method == "POST":
+        temperature = request.form.get("temperature")
+        #Remember to validate users input, don't convert to float straight away
+        temperature = float(temperature)
+        v_g = interpolate(temperature)
+        return render_template("result.html", v = v_g)
+    
+    else:
+        return render_template("specific.html")
---
+++ functions.py
@@ -0,0 +1,50 @@
+# Functions used to interpolate
+import numpy as np
+
+from flask import redirect, render_template, session
+from functools import wraps
+
+data = np.array([[0.5,81.3,3.239],
+    [1,99.6,1.694],
+    [10,179.9,0.1944],
+    [20,212.4,0.09957],
+    [40,250.3,0.04977],
+    [100,311,0.01802],
+    [150,342.1,0.01035],
+    [180,357,0.00751],
+    [200,365.7,0.00585],
+    [220,373.7,0.00368]])
+
+P_data = data[:,0]
+Tsat_data = data[:,1]
+vg_data = data[:,2]
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def interpolate(T):
+    # Calculate specific volume using ideal gas equation, given Temperature
+    # Assume given Temperature (do one for given Pressure?)
+    # Assume given R&M table and need to interpolate to get Pressure if Temperature NOT in the table
+    # numpy.interp(x, xp, fp)
+    # x = x-coordinates at which to evaluate the interpolated values
+    # xp = x-coordinates of the data points, must be increasing if the argument period is not specified
+    # fp = y-coordinates of the data points, same length as xp
+    P_bar = np.interp(T,Tsat_data,P_data)
+    R = 461.5
+    P_Pa = P_bar * 100000
+    vg = R * (T+273.15) / P_Pa
+    
+    return vg
+
